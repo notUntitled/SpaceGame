d71c56f253691dc299399a612c8a641223a4b6db
Yannis Lagrosa <378940@ga.aliefisd.net>
2022-07-11 19:55:59 -0500

    Updated Files.

--- New Unity Project/Assets/EnemySpawner.cs
+++ New Unity Project/Assets/EnemySpawner.cs
@@ -5,15 +5,24 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     public float TimeOfSpawn;
-    public float SpawnTime;
-
+    public float SpawnTime = 5f;
+    public GameObject enemy;
     private void Start()
     {
-        TimeOfSpawn = Time.time;
-        SpawnTime = Time.time;
+        TimeOfSpawn = 0;
     }
     void Update()
     {
-        
+        if (TimeOfSpawn + Time.time >= SpawnTime + Time.time)
+        {
+            TimeOfSpawn = 0;
+
+        }
+        TimeOfSpawn += Time.deltaTime;
+    }
+
+    public void SpawnEnemy()
+    {
+        GameObject enemyl = GameObject.Instantiate<GameObject>(enemy);
     }
 }
